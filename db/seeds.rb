d7a88cfcc2b0c579d7f1610c744bdea9c3929934
Maelle Rudner Mercier <mazz.r.m@gmail.com>
2023-10-21 12:35:08 -0400

    new changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do |i|
+  movie = Movie.create(
+    title: Faker::Book.title,
+    overview: Faker::Lorem.paragraph,
+    poster_url: Faker::Avatar.image,
+    rating: rand(1.0..10.0).round(2)
+  )
+end
